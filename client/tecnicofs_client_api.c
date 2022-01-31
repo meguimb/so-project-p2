2d6a96082da3175cfdf0205d687776ede8c5f9d5
mparreirinha <96299557+mparreirinha@users.noreply.github.com>
2022-01-31 19:05:05 +0000

    client

--- client/tecnicofs_client_api.c
+++ client/tecnicofs_client_api.c
@@ -1,27 +1,58 @@
 #include "tecnicofs_client_api.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int session_id;
+int pipe;
+int serverpipe;
+char*pipe_path;
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
-    /* TODO: Implement this */
     if (unlink(client_pipe_path)!=0) {
         return -1;
     }
     if (mkfifo(client_pipe_path,0640)!=0) {
         return -1;
     }
-    int server = open(server_pipe_path, O_WRONLY);
-    if (server == -1) {
+    serverpipe = open(server_pipe_path, O_WRONLY);
+    if (serverpipe == -1) {
         return -1;
     }
-    
-    int client = open(client_pipe_path, O_RDONLY);
-    if (client == -1) {
+    strcpy(pipe_path,client_pipe_path);
+    char* msg = TFS_OP_CODE_MOUNT;
+    strcat(msg," | ");
+    strcat(msg,client_pipe_path);
+    size_t len = strlen(msg);
+    if (write(server,msg,len) < 0) {
+        return -1;
+    }
+    pipe = open(client_pipe_path, O_RDONLY);
+    if (pipe == -1) {
+        return -1;
+    }
+    if (read(client,&session_id,1) < 0) {
         return -1;
     }
     return 0;
 }
 
 int tfs_unmount() {
-    /* TODO: Implement this */
+    char* msg = TFS_OP_CODE_UNMOUNT;
+    strcat(msg, " | ");
+    strcat(msg,session_id);
+    if (write(server,msg,len) < 0) {
+        return -1;
+    }
+    if (close(pipe) < 0) {
+        return -1;
+    }
+    if (close(serverpipe) < 0) {
+        return -1;
+    }
+    if (unlink(pipe_path)!=0) {
+        return -1;
+    }
     return -1;
 }
 
