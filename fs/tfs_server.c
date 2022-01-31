b38d6c624243cb6a10e6ceab4ed89d66479d2ec7
mparreirinha <96299557+mparreirinha@users.noreply.github.com>
2022-01-31 19:05:32 +0000

    Add files via upload

--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -1,4 +1,7 @@
 #include "operations.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 
@@ -22,6 +25,47 @@ int main(int argc, char **argv) {
 
     if (tfs_init()!=0) {
         exit(1);
+    }
+
+    int pipe = open(pipename, O_RDONLY);
+
+    if (pipe < 0) {
+        exit(1);
+    }
+
+    int clientpipe;
+    int session_id = 0;
+
+    for(;;) {
+        char opcode;
+        if (read(pipename,opcode,sizeof(char)) < 0) {
+            return -1;
+        }
+        if (opcode == TFS_OP_CODE_MOUNT) {
+            char readed[40]
+            if (read(pipename,readed,40)<0) {
+                return -1;
+            }
+            int clientpipe = open(client_pipe_path, O_WRONLY);
+            if (clientpipe == -1) {
+                return -1;
+            }
+            if (write(clientpipe, &session_id, 1) < 0) {
+                return -1;
+            }
+        }
+        else if (opcode == TFS_OP_CODE_UNMOUNT) {
+            int session_id_r;
+            if (read(pipename,&session_id_r,1)<0) {
+                return -1;
+            }
+            if (session_id_r != session_id) {
+                return -1;
+            }
+            if (close(clientpipe) < 0) {
+                return -1;
+            }
+        }
     }
 
     return 0;
