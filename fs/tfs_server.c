9051bd4876d4436760d89770ad9aa1ab726805a7
Margarida Bezerra <megui123455@gmail.com>
2022-02-01 13:41:20 +0000

    fixed unlink bug, server now gets in a loop

--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char **argv) {
 
@@ -15,26 +16,26 @@ int main(int argc, char **argv) {
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
     /* TO DO */
-
-    if (unlink(pipename)!= 0) {
+    // API -> write no pipe_server e read do client_server
+    // SERVER -> read do pipe_server e write no client_server
+    if (unlink(pipename) != 0 && errno != ENOENT) {
+        exit(EXIT_FAILURE);
+    }
+    if (mkfifo(pipename, 0640) < 0) {
         exit(1);
     }
-
-    if (mkfifo(pipename, 0640)!=0) {
-        exit(1);
-    }
-
+    printf("before tfs_init\n");
     if (tfs_init()!=0) {
         exit(1);
     }
+    printf("tfs server opening server pipe\n");
+    int pipe_server = open(pipename, O_RDONLY);
 
-    int pipe = open(pipename, O_RDONLY);
-
-    if (pipe < 0) {
+    if (pipe_server < 0) {
         exit(1);
     }
 
-    int clientpipe;
+    int pipe_client;
     int session_id = 0;
     char *client_pipe_path;
     while(true) {
@@ -47,11 +48,11 @@ int main(int argc, char **argv) {
             if (read(pipename,readed,40)<0) {
                 return -1;
             }
-            int clientpipe = open(client_pipe_path, O_WRONLY);
-            if (clientpipe == -1) {
+            pipe_client = open(client_pipe_path, O_WRONLY);
+            if (pipe_client == -1) {
                 return -1;
             }
-            if (write(clientpipe, &session_id, 1) < 0) {
+            if (write(pipe_client, &session_id, 1) < 0) {
                 return -1;
             }
         }
@@ -63,26 +64,73 @@ int main(int argc, char **argv) {
             if (session_id_r != session_id) {
                 return -1;
             }
-            if (close(clientpipe) < 0) {
+            if (close(pipe_client) < 0) {
                 return -1;
             }
         }
         else if (opCode == TFS_OP_CODE_READ){
-            // read (int) session id => 6 bytes
-
-            // read (int) fhandle => 6 bytes
-
-            // read (size_t) len => 8 bytes
-
+            int session_id_atual, fhandle;
+            size_t len;
+            char *readed;
+            // read (int) session id
+            if (read(pipe_server, &session_id_atual, sizeof(int)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            // read (int) fhandle
+            if (read(pipe_server, &fhandle, sizeof(int)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            // read (size_t) len
+            if (read(pipe_server, &len, sizeof(size_t)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            readed = (char *) malloc(len);
+            // chamar tfs_read do operations
+            if (tfs_read(fhandle, readed, len) != len){
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            // devolver info sobre como correu esta operação ao cliente
+            // TODO
+            write(pipe_client, 0, sizeof(int));
+            return 0;
         }
         else if (opCode == TFS_OP_CODE_WRITE){
+            int session_id_atual, fhandle;
+            size_t len;
             // read (int) session_id
-
+            if (read(pipe_server, &session_id_atual, sizeof(int)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
             // read (int) fhandle
-
+            if (read(pipe_server, &fhandle, sizeof(int)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
             // read (size_t) len
-
+            if (read(pipe_server, &len, sizeof(size_t)) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            char buf [len];
             // read char [len] buffer
+            if (read(pipe_server, buf, len) < 0) {
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            // chamar tfs_write
+            if (tfs_write(fhandle, buf, len) != len){
+                write(pipe_client, -1, sizeof(int));
+                return 0;
+            }
+            // devolver info sobre como correu esta operação ao cliente
+            // TODO
+            write(pipe_client, 0, sizeof(int));
+            return 0;
         }
     }
 
