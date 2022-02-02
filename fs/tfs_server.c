cfa9d4f6a155d8b90607f1bf9433dad65281ea55
Margarida Bezerra <megui123455@gmail.com>
2022-02-02 13:06:31 +0000

    added read_int, read_name and read_size_t functions

--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -10,6 +10,9 @@
 #include <unistd.h>
 
 int session_id_c;
+char *read_name(int pipe_server);
+int read_int(int pipe_server, int pipe_client);
+size_t read_size_t(int pipe_server, int pipe_client);
 
 int main(int argc, char **argv) {
 
@@ -45,7 +48,7 @@ int main(int argc, char **argv) {
     }
 
     int pipe_client;
-    char *client_pipe_path = (char *) malloc(40);
+    char *client_pipe_path;
     while(true) {
         char opCode;
         //read(pipe_server, &opCode, sizeof(char));
@@ -54,17 +57,18 @@ int main(int argc, char **argv) {
             return -1;
         }
         if (opCode == TFS_OP_CODE_MOUNT) {
-            char readed[40];
-            if (read(pipe_server, readed, 40) < 0) {
-                return -1;
-            }
-            memcpy(client_pipe_path, readed, strlen(readed)+1);
-            client_pipe_path[strlen(readed)] = '\0';
-            pipe_client = open(client_pipe_path, O_WRONLY);
+            // read and set client pipe path
+            client_pipe_path = read_name(pipe_server);
+
+            // set session_id for this new session
             session_id = session_id_c;
+
+            // opening client pipe and sending client new session id
+            pipe_client = open(client_pipe_path, O_WRONLY);
             if (write(pipe_client, &session_id, sizeof(int)) < 0) {
                 return -1;
             }
+            close(pipe_client);
             session_id_c++;
         }
         else if (opCode == TFS_OP_CODE_UNMOUNT) {
@@ -83,91 +87,44 @@ int main(int argc, char **argv) {
             int session_id_atual, fhandle;
             size_t len;
             char *readed;
+
             // read (int) session id
-            if (read(pipe_server, &session_id_atual, sizeof(int)) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            // read (int) fhandle
-            if (read(pipe_server, &fhandle, sizeof(int)) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            // read (size_t) len
-            if (read(pipe_server, &len, sizeof(size_t)) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            readed = (char *) malloc(len);
-            // chamar tfs_read do operations
-            if (tfs_read(fhandle, readed, len) != len){
-                ret_val = -1;   
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
+            session_id_atual = read_int(pipe_server, pipe_client);
+            fhandle = read_int(pipe_server, pipe_client);
+            len = read_size_t(pipe_server, pipe_client);
+            readed = read_name(pipe_server);
             // devolver info sobre como correu esta operação ao cliente
-            // TODO
-            ret_val = 0;
+            pipe_client = open(client_pipe_path, O_WRONLY);
+            ret_val = tfs_read(fhandle, readed, len);
             if (write(pipe_client, &ret_val, sizeof(int)) < 0){
+                close(pipe_client);
                 return -1;
             }
+            close(pipe_client);
             return 0;
         }
         else if (opCode == TFS_OP_CODE_OPEN){
-            printf("opCode OPEN\n");
             int session_id_atual, flags;
-            char name [40];
             char *name_ptr;
-            close(pipe_server);
-            pipe_server = open(pipename, O_RDONLY);
-            // read (int) session_id
-            if (read(pipe_server, &session_id_atual, sizeof(int)) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            // read char name [40]
-            if (read(pipe_server, name, 40) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            size_t len = strlen(name);
-            printf("name is: %s\n", name);
-            name_ptr = malloc(len+1);
-            memcpy(name_ptr, name, len+1);
-            name_ptr[len] = '\0';
-            // read flags
-            if (read(pipe_server, &flags, sizeof(int)) < 0) {
-                ret_val = -1;
-                if (write(pipe_client, &ret_val, sizeof(int)) < 0){
-                    return -1;
-                }
-                return 0;
-            }
-            printf("doing tfs_open\n");
-            printf("session_id is %d name is %s and flag is %d\n", session_id, name_ptr, flags);
+            
+            // opening client pipe
+            pipe_client = open(client_pipe_path, O_WRONLY);
+
+            // read info from pipe
+            session_id_atual = read_int(pipe_server, pipe_client);
+            name_ptr = read_name(pipe_server);     
+            flags = read_int(pipe_server, pipe_client);
+
+            // perform operation
             ret_val = tfs_open(name_ptr, flags);
-            printf("ret_val from tfs_open operations is %d\n", ret_val);
+
+            // send return code to client
             if (write(pipe_client, &ret_val, sizeof(int)) < 0){
+                close(pipe_client);
                 return -1;
             }
-            printf("server: leaving tfs_open\n");
+            // closing client pipe
+            close(pipe_client);
         }
         else if (opCode == TFS_OP_CODE_WRITE){
             int session_id_atual, fhandle;
@@ -222,4 +179,40 @@ int main(int argc, char **argv) {
     }
 
     return 0;
+}
+
+char *read_name(int pipe_server){
+    char readed[40];
+    char *name;
+    if (read(pipe_server, readed, 40) < 0) {
+        return -1;
+    }
+    name = malloc(strlen(readed)+1);
+    memcpy(name, readed, strlen(readed)+1);
+    return name;
+}
+
+int read_int(int pipe_server, int pipe_client){
+    int val, ret_val;
+    if (read(pipe_server, &val, sizeof(int)) < 0) {
+        ret_val = -1;
+        if (write(pipe_client, &ret_val, sizeof(int)) < 0){
+            return -1;
+        }
+        return -1;
+    }
+    return val;
+}
+
+size_t read_size_t(int pipe_server, int pipe_client){
+    size_t len;
+    int ret_val;
+    if (read(pipe_server, &len, sizeof(size_t)) < 0) {
+        ret_val = -1;
+        if (write(pipe_client, &ret_val, sizeof(int)) < 0){
+            return 0;
+        }
+        return 0;
+    }
+    return len;
 }
